5fdda75e8657a1a96e9f2b8ed84a98232d56e5fb
UG <utsav.ghosh.0110@gmail.com>
2020-08-12 03:50:25 +0530

    Final Commit.

---
+++ CubeOf6/src/CubeOf6Code.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author UG
+ *
+ */
+public class CubeOf6Code {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Cube of 6 is : "+ Math.pow(6, 3));
+
+	}
+
+}
