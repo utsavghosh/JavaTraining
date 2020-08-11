e70edcf4f70b096d7c4cf77a7ffb1bb8c96a4b3d
UG <utsav.ghosh.0110@gmail.com>
2020-08-12 04:16:56 +0530

    Final commit.

---
+++ EvenOrOdd/src/EvenOrOddCode.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author UG
+ *
+ */
+public class EvenOrOddCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int number = 7;
+		if (number % 2 == 0)
+			System.out.println(number + " is Even");
+		System.out.println(number + " is Odd"); 
+
+	}
+
+}
