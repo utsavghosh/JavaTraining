3dc64d7e2da57c7ce62b0a22345e3f925577f80f
UG <utsav.ghosh.0110@gmail.com>
2020-08-12 03:40:48 +0530

    Final commit.

---
+++ PrintIntTill20/src/PrintIntTill20Code.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author UG
+ *
+ */
+public class PrintIntTill20Code {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i = 0;i<=20;i++)
+			System.out.println(i);
+
+	}
+
+}
