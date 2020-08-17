d2cbf43b644f6c58526d954d2dcde7f21d058192
UG <utsav.ghosh.0110@gmail.com>
2020-08-17 16:12:15 +0530

    Added current account details.

--- Class5_BankAccount/src/Class5/CurrentAccount.java
+++ Class5_BankAccount/src/Class5/CurrentAccount.java
@@ -1,5 +1,11 @@
 package Class5;
 
-public class CurrentAccount {
+public class CurrentAccount extends Account {
+	//double interestRate;
+
+    public CurrentAccount(int acctNum, String name) {
+        super(acctNum, name);
+        this.interestRate = 2.0;
+    }
 
 }
