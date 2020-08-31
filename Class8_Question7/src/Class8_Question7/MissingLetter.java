9903cb4d71a894c7cd890fd4ba5160faa5ac25c4
UG <utsav.ghosh.0110@gmail.com>
2020-09-01 02:03:34 +0530

    Final Commit.

--- Class8_Question7/src/Class8_Question7/MissingLetter.java
+++ Class8_Question7/src/Class8_Question7/MissingLetter.java
@@ -1,47 +1,32 @@
 package Class8_Question7;
 import java.util.*;
+
+import com.sun.jdi.CharType;
 //please help me with the logic. Mine looks flawed
 
 public class MissingLetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//String arr[];
-		final String [] arr= new String [] {"a","b","c","d","e","f","g",
-			    "h","i","j","k","l","m","n",
-			    "o","p","q","r","s","t","u",
-			    "v","w","x","y","z"
-		};
-		System.out.println("Enter number of characters in sequence");
 		Scanner scan = new Scanner (System.in);
-		int i = scan.nextInt();
-		String[] arr1 = new String[i];
-		
-		for(int j = 0; j<i;j++) {
-			System.out.println("Enter the letter sequence");
-			arr1[j]=scan.next();
-		}	
-			
-		int counter =0;
-		String s;
-		for (int j=0;j<26;j++) {
-			if(arr[j]==arr1[j]) {
-				for(int l = 0;l<i;l++) {
-					if(arr[l]==arr1[l]) {
-						counter++;
-						 
-					}
-					else 
-						s = arr[l];
-				}
-				
+		System.out.println("Enter the sequence like a word");
+		String s = scan.next();
+		int i = s.length();
+		char [] arr1 = new char[i];
+		for(int j=0; j<i;j++) 
+			arr1[j]=s.charAt(j);
+		int a, b;
+		for (int j=0;j<i-1;j++) {
+			a=arr1[j];
+			b=arr1[j+1];
+			while(b-a>1) {
+				System.out.println("Missing element is "+(char)(a+1));
+				a++;
 			}
 		}
-		if (counter < i)
-			System.out.println("Missing letter in sequence is "+ s);
-		else 
-			System.out.println("Letters are in sequence");
+		
+		
 
-	}
+		
+	}		
 
 }
