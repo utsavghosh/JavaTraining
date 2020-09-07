bef35b4c2962592667e948432ec1ac5c44c58eff
BYO <BYO@192.168.1.5>
2020-09-07 16:29:44 +0530

    Final

---
+++ Class12_FileExistsOrNot/src/FileExistsOrNot/FileExistsOrNot.java
@@ -0,0 +1,25 @@
+package FileExistsOrNot;
+import java.io.File;
+import java.io.IOException;
+
+public class FileExistsOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	      
+	    	  File file = new File("C:\\");
+	          System.out.println(file.exists());
+	       
+	      
+		
+
+	}
+
+}
+
+
+ 
+
+      
+
