48f6be6655bf682dc627d303849c80442f1aaa73
UG <utsav.ghosh.0110@gmail.com>
2020-10-07 02:56:44 +0530

    Final commit.

---
+++ Class 16_Question2/src/InsertArrayList/InsertArrayList.java
@@ -0,0 +1,27 @@
+package InsertArrayList;
+
+import java.util.ArrayList;
+
+public class InsertArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      ArrayList<String> alist=new ArrayList<String>();  
+	      alist.add("Nakul");
+	      alist.add("Uru");
+	      alist.add("Eshu");
+	      alist.add("Feba");
+	      alist.add("Sanja");
+	  
+	      //displaying elements
+	      System.out.println(alist);
+	  
+	      //Adding "Utsav" at the first position
+	      alist.add(0, "Utsav");
+	  
+	      //displaying elements
+	      System.out.println(alist);
+
+	}
+
+}
